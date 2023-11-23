459ea5e4ef3597fc0ea67f36a3a9f48e909fcdcf
Dhruv Karia <dhruvkaria@hotmail.com>
2023-11-22 21:42:01 -0800

    done

--- app/src/main/java/edu/uw/ischool/dkaria/awty/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/dkaria/awty/MainActivity.kt
@@ -1,9 +1,14 @@
 package edu.uw.ischool.dkaria.awty
+
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import java.util.*
 
 class MainActivity : ComponentActivity() {
@@ -26,31 +31,44 @@ class MainActivity : ComponentActivity() {
 
         startStopButton.setOnClickListener {
             if (startStopButton.text == "Start") {
-                val message = messageEditText.text.toString()
-                val phoneNumber = phoneNumberEditText.text.toString()
-                val minutes = minutesEditText.text.toString().toIntOrNull()
-
-                if (message.isEmpty() || phoneNumber.isEmpty() || minutes == null || minutes <= 0) {
-                    Toast.makeText(this, "Please fill out all fields with valid values.", Toast.LENGTH_SHORT).show()
-                } else if (phoneNumber.any { it.isLetter() }) {
-                    Toast.makeText(this, "Letters are not allowed in the phone number section", Toast.LENGTH_SHORT).show()
+                if (ContextCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.SEND_SMS
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(Manifest.permission.SEND_SMS),
+                        1
+                    )
                 } else {
-                    startStopButton.text = "Stop"
-                    timer = Timer()
-                    timer?.scheduleAtFixedRate(object : TimerTask() {
-                        override fun run() {
-                            runOnUiThread {
-                                val toast = Toast.makeText(this@MainActivity, "", Toast.LENGTH_LONG)
-                                toast.setText("Texting $phoneNumber: $message")
-                                toast.show()
-                            }
-                        }
-                    }, 0, (minutes * 60 * 1000).toLong())
+                    startSendingSMS()
                 }
             } else {
                 startStopButton.text = "Start"
                 timer?.cancel()
             }
+        }
+    }
+
+    private fun startSendingSMS() {
+        val message = messageEditText.text.toString()
+        val phoneNumber = phoneNumberEditText.text.toString()
+        val minutes = minutesEditText.text.toString().toIntOrNull()
+
+        if (message.isEmpty() || phoneNumber.isEmpty() || minutes == null || minutes <= 0) {
+            Toast.makeText(this, "Please fill out all fields with valid values.", Toast.LENGTH_SHORT).show()
+        } else if (phoneNumber.any { it.isLetter() }) {
+            Toast.makeText(this, "Letters are not allowed in the phone number section", Toast.LENGTH_SHORT).show()
+        } else {
+            startStopButton.text = "Stop"
+            timer = Timer()
+            timer?.scheduleAtFixedRate(object : TimerTask() {
+                override fun run() {
+                    val smsManager = android.telephony.SmsManager.getDefault()
+                    smsManager.sendTextMessage(phoneNumber, null, message, null, null)
+                }
+            }, 0, (minutes * 60 * 1000).toLong())
         }
     }
 }
